a878f364556d1f872da3053836a60069cff391f4
grg909 <grg909@foxmail.com>
2019-03-02 03:09:06 +0900

    add language

---
+++ app/cli.py
@@ -0,0 +1,32 @@
+from app import app
+import os, click
+
+@app.cli.group()
+def translate():
+    """翻译和本地化相关命令。"""
+    pass
+
+@translate.command()
+def update():
+    """更新所有语言"""
+    if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
+        raise RuntimeError('extract command failed')
+    if os.system('pybabel update -i messages.pot -d app/translations'):
+        raise RuntimeError('update command failed')
+    os.remove('message.pot')
+
+@translate.command()
+def compile():
+    """编译语言文件"""
+    if os.system('pybabel compile -d app/translations'):
+        raise RuntimeError('compile command failed')
+
+@translate.command()
+@click.argument('lang')
+def init(lang):
+    """安装新语言"""
+    if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
+        raise RuntimeError('extract command failed')
+    if os.system('pybabel init -i messages.pot -d app/translations -l' + lang):
+        raise RuntimeError('init command failed')
+    os.remove('messages.pot')
