89735b33c2a39548c4b0e601080b8424394632aa
grg909 <grg909@foxmail.com>
2019-03-03 21:43:46 +0900

    v16.0 beautify

--- app/main/routes.py
+++ app/main/routes.py
@@ -133,3 +133,10 @@ def translate_text():
     return jsonify({'text': translate(request.form['text'],
                                       request.form['source_language'],
                                       request.form['dest_language'])})
+
+
+@bp.route('/user/<username>/popup')
+@login_required
+def user_popup(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user_popup.html', user=user)
