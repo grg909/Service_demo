7d261d437b546e66e855205ab54d973faf858634
grg909 <grg909@foxmail.com>
2020-01-01 21:30:20 +0900

    modify default limit

--- app/limiter.py
+++ app/limiter.py
@@ -12,4 +12,4 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
 
-limiter = Limiter(key_func=get_remote_address, default_limits=["300 per day", "60 per hour"])
+limiter = Limiter(key_func=get_remote_address, default_limits=["1 per second"])
